52deb9c65a29258af593c8b9a5dd4aca0ccf92e8
Arene Amezua <aamezua005@ikasle.ehu.eus>
2026-02-16 20:26:32 +0100

    #3 #5 #10 ✅

--- DBManager/Condition.cs
+++ DBManager/Condition.cs
@@ -15,6 +15,9 @@ namespace DbManager
         public Condition(string column, string op, string literalValue)
         {
             //TODO DEADLINE 1A: Initialize member variables
+            ColumnName = column;
+            Operator = op;
+            LiteralValue = literalValue;
             
         }
 
@@ -27,10 +30,58 @@ namespace DbManager
             //"9" > "10"
             //9 < 10
             //Convert first the strings to the appropriate type and then compare (depending on the operator of the condition)
+            switch (type)
+            {
+                case ColumnDefinition.DataType.String:
+                    return ComparaStrings(value, LiteralValue);
 
-            
-            return false;
+                case ColumnDefinition.DataType.Int:
+                    int intValue = int.Parse(value, CultureInfo.InvariantCulture);
+                    int intLiteral = int.Parse(LiteralValue, CultureInfo.InvariantCulture);
+                    return ComparaNumeros(intValue, intLiteral);
+
+                case ColumnDefinition.DataType.Double:
+                    double doubleValue = double.Parse(value, CultureInfo.InvariantCulture);
+                    double doubleLiteral = double.Parse(LiteralValue, CultureInfo.InvariantCulture);
+                    return ComparaNumeros(doubleValue, doubleLiteral);
+
+                default:
+                    return false;
+            }
             
         }
+
+        private bool ComparaStrings(string a, string b)
+        {
+            int cmp = string.Compare(a, b, StringComparison.Ordinal);
+
+            return Operator switch
+            {
+                "="  => cmp == 0,
+                "!=" => cmp != 0,
+                "<"  => cmp < 0,
+                "<=" => cmp <= 0,
+                ">"  => cmp > 0,
+                ">=" => cmp >= 0,
+                _ => false
+            };
+        }
+
+        private bool ComparaNumeros<T>(T a, T b) where T : IComparable<T>
+        {
+            int cmp = a.CompareTo(b);
+
+            return Operator switch
+            {
+                "="  => cmp == 0,
+                "!=" => cmp != 0,
+                "<"  => cmp < 0,
+                "<=" => cmp <= 0,
+                ">"  => cmp > 0,
+                ">=" => cmp >= 0,
+                _ => false
+            };
+        }
+
     }
 }
