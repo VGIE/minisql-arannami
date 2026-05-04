435530f3b10ed6e4c0a42737d0ff46edbe73ba00
Naroa Solabarrieta <naroasolabarrieta@gmail.com>
2026-05-04 10:43:00 +0200

    #42 method update changes in regex and parsing

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -21,7 +21,7 @@ namespace DbManager
             //And then, an execution error should be given if a CreateTable without columns is executed
             const string createTablePattern = @"^CREATE\s+TABLE\s+([a-zA-Z]\w*)\s*\((.*)\)$";
 
-            const string updateTablePattern = @"^UPDATE\s+(\w+)\s+SET\s+(.+?)(?:\s+WHERE\s+(\w+)(=|<>|<|>|<=|>=)('[^']*'|\d+))?$";
+            const string updateTablePattern = @"^UPDATE\s+(\w+)\s+SET\s+(.+?)(?:\s+WHERE\s+(\w+)\s*(<=|>=|<>|=|<|>)\s*('[^']*'|\d+))?\s*$";
 
             const string deletePattern = @"^DELETE\s+FROM\s+(\w+)(?:\s+WHERE\s+(\w+)(=|<>|<=|>=|<|>)('[^']*'|\d+(?:\.\d+)?))?\s*;?$";
 
@@ -175,7 +175,7 @@ namespace DbManager
                 {
                     var setMatch = Regex.Match(
                         assignment.Trim(),
-                        @"^(\w+)=('[^']*'|\d+)$"
+                        @"^(\w+)\s*=\s*('[^']*'|\d+)$"
                     );
 
                     if (!setMatch.Success)
